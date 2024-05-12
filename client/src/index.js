3d21f9f0c6cb0e67665dbf58a3dd2b89ad7c4b5c
T-Rzeznik <trzeznik29@gamil.com>
2024-05-12 16:35:40 -0400

    updated interface

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
@@ -10,15 +10,45 @@ import CreateRoom from './CreateRoom';
 import UpdateRoom from './UpdateRoom';
 import DeleteRoom from './DeleteRoom';
 
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="nav-container">
+        <nav>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Rooms List</Link>
+            </li>
+            <li>
+              <Link to="/create">Create Room</Link>
+            </li>
+            <li>
+              <Link to="/update/:id">Update Room</Link>
+            </li>
+            <li>
+              <Link to="/delete/:id">Delete Room</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="component-container">
+        <Routes>
+          <Route path="/" element={<RoomsList />} />
+          <Route path="/room/:id" element={<RoomDetails />} />
+          <Route path="/create" element={<CreateRoom />} />
+          <Route path="/update/:id" element={<UpdateRoom />} />
+          <Route path="/delete/:id" element={<DeleteRoom />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <RoomsList />
-    <RoomDetails />
-    <CreateRoom />
-    <UpdateRoom />
-    <DeleteRoom />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
